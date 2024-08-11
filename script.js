d4eef9dc159265c44b9f0d531106e6bdd8293a29
Sebastian Arce <sebastianarce25@gmail.com>
2024-08-11 21:59:15 +0100

    add some basic functions

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ issac_birth_year = showMessage("Issac", 3)
 console.log(message)
 console.log(travi_birth_year)
 console.log(issac_birth_year)
+
+
+function add7(number) {
+    return number + 7;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function capitalize(text) {
+    text = text.toLowerCase()
+    text = text.charAt(0).toUpperCase() + text.slice(1)
+    return text
+}
+
+function lastLetter(text) {
+    return text.slice(-1)
+}
+
+console.log("add7: " + add7(7))
+console.log("multiply: " + multiply(7, 7))
+console.log("capitalize: " + capitalize("BoTh"))
+console.log("lastLetter: " + lastLetter("abcd"))
