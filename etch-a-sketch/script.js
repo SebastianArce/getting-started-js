bb439150de1a9023e16d339a49d06165b5cfef54
Sebastian Arce <sebastianarce25@gmail.com>
2024-08-26 09:59:58 +0100

    create grid with two for loops, instead of flex wrap

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -7,33 +7,35 @@ body.style.margin = 0;
 
 function createGrid(gridDimension) {
   container.style.display = "flex";
-  container.style.justifyContent = "space-around";
-  container.style.flexWrap = "wrap";
-  container.style.gap = "1vw"
+  container.style.flexDirection = "column";
+  container.style.gap = "1vw";
   container.style.backgroundColor = "#F7F3E3";
-  container.style.flexGrow = 1;
-  
-  // We are substracting gridDimension to allow space for gap
-  let boxWidth = Math.floor((100 - gridDimension) / gridDimension);
-  let gridArea = gridDimension * gridDimension
 
-  for (let j = 0; j < gridArea; j++) {
-    // add boxes to container
-    let box = document.createElement("div");
-    box.style.width = `${boxWidth}%`;
-    box.style.aspectRatio = 1 / 1;
-    box.style.backgroundColor = "#AF9164";
-    container.appendChild(box);
+  let boxWidth = Math.floor(100 / gridDimension);
 
-    box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = "#2B2118"
-    })
+  for (let i = 0; i < gridDimension; i++) {
+    let row = document.createElement("div");
+    row.style.display = "flex";
+    row.style.justifyContent = "space-around";
+    row.style.gap = "1vw";
+
+    for (let j = 0; j < gridDimension; j++) {
+      // add boxes to container
+      let box = document.createElement("div");
+      box.style.width = `${boxWidth}%`;
+      box.style.aspectRatio = 1 / 1;
+      box.style.backgroundColor = "#AF9164";
+      row.appendChild(box);
+
+      box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "#6F1A07";
+      });
+    }
+    container.appendChild(row);
   }
 }
 
-function draw() {
+function draw() {}
 
-}
-
-const gridDimension = 16;
+const gridDimension = 24;
 createGrid(gridDimension);
